35cb6ca5e1ae2e3fd8d71c0004d07ec70c554130
alisonnRB <alisonrbjs@gmail.com>
2024-05-02 21:08:00 -0300

    ready

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Console.WriteLine("Smartphone Nokia: ");
+Smartphone nokia = new Nokia(numero: "123456", modelo: "plpl", imei: "oooo", memoria: 121);
+nokia.Ligar();
+nokia.InstalarAplicativo("WhatsApp");
+
+Console.WriteLine("Smartphone Iphone: ");
+Smartphone iphone = new Iphone(numero: "123323456", modelo: "uypl", imei: "iioo", memoria: 200);
+iphone.Ligar();
+iphone.InstalarAplicativo("linkedin");
